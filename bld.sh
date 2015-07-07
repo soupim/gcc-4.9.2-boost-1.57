4131bfdd111de34de6dc33ec4ff0369df126a90c
imcs <imcs@telcoware.com>
2015-07-07 15:02:08 +0900

    boost 1.57.0 -> boost 1.58.0 change path of mpfr-3.1.2

--- bld.sh
+++ bld.sh
@@ -309,13 +309,13 @@ function my-readlink
 ARS=(
     http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.14.tar.gz
     https://gmplib.org/download/gmp/gmp-6.0.0a.tar.bz2
-    http://www.mpfr.org/mpfr-current/mpfr-3.1.2.tar.bz2
+    http://ftp.gnu.org/gnu/mpfr/mpfr-3.1.2.tar.bz2
     http://www.multiprecision.org/mpc/download/mpc-1.0.2.tar.gz
     http://bugseng.com/products/ppl/download/ftp/releases/1.1/ppl-1.1.tar.bz2
     http://www.bastoul.net/cloog/pages/download/cloog-0.18.1.tar.gz
     http://ftp.gnu.org/gnu/gcc/gcc-4.9.2/gcc-4.9.2.tar.bz2
     http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.bz2
-    http://sourceforge.net/projects/boost/files/boost/1.57.0/boost_1_57_0.tar.bz2
+    http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.bz2
     https://googledrive.com/host/0B6NtGsLhIcf7MWxMMF9JdTN3UVk/gperftools-2.2.90.tar.gz
 
     #
@@ -803,3 +803,4 @@ EOF
 
     popd
 fi
+
